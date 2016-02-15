66e3f9a5a6e71c0a580ea917b4f006bd0298a34e
Louie ohhyeah <louiebasalo7@gmail.com>
2016-02-15 17:20:40 +0800

    Make Changes

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,30 +1,26 @@
 class AuthsController < ApplicationController
-  layout 'application'
   def login
     if logged_in?
       redirect_to root_path
     else
       render 'login'
     end
-
   end
   def verify
     user = User.find_by(email: params[:user][:email].downcase)
-    #render plain: user && user.authenticate(params[:user] [:password])? true : false
     if user && user.authenticate(params[:user][:password])
       log_in user
       redirect_to root_path
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right !
+      flash[:danger] = 'Invalid email/password combination'
       render 'login'
     end
   end
-
-
-def logout
-  session.destroy
-  redirect_to root_path
+  def logout
+    session.destroy
+    redirect_to root_path
+  end
 end
-end
+
 
 
